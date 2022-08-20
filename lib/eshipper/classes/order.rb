368d507f3b7b4757dc749130827dbf40b8430971
Marc Beaupré <hrdwdmrbl@gmail.com>
2022-08-20 18:35:24 +0200

    Fix: Sometimes no search results are returned

--- lib/eshipper/classes/order.rb
+++ lib/eshipper/classes/order.rb
@@ -3,8 +3,25 @@
 module EShipper
   class Order < EShipperResponse
     def self.search(options = {})
-      orders = SearchReply.search(options).OrderSearchReply.Order
-      orders.is_a?(Array) ? orders : [orders]
+      orders = SearchReply.search(options).OrderSearchReply
+
+      if orders == "\n" # No results
+        []
+      elsif orders.Order.is_a?(Array) # Multiple results
+        orders.Order
+      else # Single result
+        [orders.Order]
+      end
+    end
+
+    def references
+      if self.References.Reference == "\n"
+        []
+      elsif self.References.Reference.is_a?(Array)
+        self.References.Reference
+      else
+        [self.References.Reference]
+      end
     end
 
     def fetch
